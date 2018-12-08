9113f73c66924ecff2add10b348ecf4466e2e766
hunsuk218 <hunsuk218@gmail.com>
2018-12-08 17:06:38 +0900

    escrow add

--- TCT/TCT.py
+++ TCT/TCT.py
@@ -6,8 +6,8 @@ class TCTDapp(IconScoreBase):
     _PERSON_NAME = '_person_name'
     _CAR_OWNER = '_car_owner'
     _REPAIR_COUNT = '_repair_count'
-    
     _REPAIR_DATA = '_repiar_data'
+    _ESCROW_ADDR = '_escrow_addr'
     
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
@@ -15,9 +15,9 @@ class TCTDapp(IconScoreBase):
         self._person_name = DictDB(self._PERSON_NAME, db, value_type=str)       #person name
         self._car_owner = DictDB(self._CAR_OWNER, db, value_type=Address)       #car Owner
         self._repair_count = DictDB(self._REPAIR_COUNT, db, value_type=int)     #count
-        
         self._repair_data = DictDB(self._REPAIR_DATA, db, value_type=str, depth=2)       #repiar Data
-        
+        self._escrow_addr = VarDB(self._ESCROW_ADDR,db, value_type=Address)
+
     def on_install(self) -> None:
         """
             when deployed then this start.
@@ -25,7 +25,7 @@ class TCTDapp(IconScoreBase):
             ex) token Address 
         """
         super().on_install()
-            
+        
     def on_update(self) -> None:
         super().on_update()
     
@@ -40,13 +40,13 @@ class TCTDapp(IconScoreBase):
             return False
         else:
             return True
-
-
-    @external(readonly=True)
-    def hello(self) -> str:
-        Logger.debug(f'Hello, world!', TAG)
-        return "Hello"
     
+    @external
+    def setEscrowAddr(self, _addr:Address):
+        if self.msg.sender != self.owner:
+            revert("It is only for owner")
+        
+        self._escrow_addr.set(_addr)
 
     @external
     def setPersonName(self, _name: str):
@@ -56,7 +56,6 @@ class TCTDapp(IconScoreBase):
             self._person_name[self.msg.sender] = _name
             Logger.debug(f'Set Person')
             
-
     @external
     def setCar(self, _carNumber: str):
         if self.isCar(_carNumber):
@@ -70,9 +69,12 @@ class TCTDapp(IconScoreBase):
         return self._car_owner[_carNumber]
     
     @external(readonly=True)
-    def getPersonName(self, _from:Address) -> str:
-        return self._person_name[_from]
+    def getPersonName(self, _addr :Address) -> str:
+        return self._person_name[_addr]
 
+    @external(readonly=True)
+    def getRepairCount(self, _carNumber) -> int:
+        return self._repair_count[_carNumber]
 
         
     @external
@@ -84,7 +86,7 @@ class TCTDapp(IconScoreBase):
             revert("regist first")
         
         count = 0
-        count = self._repair_count[_carNumber]
+        count = self.getRepairCount(_carNumber)
         count += 1
         self._repair_count[_carNumber] = count
         
@@ -98,8 +100,19 @@ class TCTDapp(IconScoreBase):
         
         self._repair_data[_carNumber][count] = repairDataStr
         Logger.debug(f'set repair data!! good!')
+    
 
     @external(readonly=True)
     def getRepairInfo(self, _carNumber:str, _carCount : int) -> str:
         return self._repair_data[_carNumber][_carCount]
 
+    @external(readonly=True)
+    def getRepairInfoDict(self, _carNumber:str, _carCount : int) -> dict:
+        return json_loads(self._repair_data[_carNumber][_carCount])
+
+    @external
+    def ownerChange(self, _from : Address, _to : Address, _carNumber : str):
+        if self.msg.sender != self._escrow_addr.get():
+            revert("It is only excuted from escrow!!")
+        else:
+            self._car_owner[_carNumber] = _to
---
+++ escrow/escrow.py
@@ -0,0 +1,141 @@
+from iconservice import *
+
+TAG = 'TCTescrow'
+
+class tctDappInterface(InterfaceScore):
+    @interface
+    def getCarOwner(self, _carNumber:str) -> Address:
+        pass
+
+    @interface
+    def getPersonName(self, _addr:Address) -> str:
+        pass
+
+    @interface
+    def ownerChange(self, _from : Address, _to : Address, _carNumber : str):
+        pass
+
+
+
+class TokenInterface(InterfaceScore):
+    @interface
+    def transfer(self, _to: Address, _value: int, _data: bytes=None):
+        pass
+    
+    @interface
+    def _transfer(self,  _from: Address, _to: Address, _value: int, _data: bytes):
+        pass
+
+
+
+class TCTescrow(IconScoreBase):
+    _ESCROW_DATA = '_escrow_data'
+    _ESCROW_COUNT = '_escrow_count'
+    _TCTDAPP_ADDR = '_tctdapp_addr'
+    _TOKEN_ADDR = '_token_addr'
+
+    def __init__(self, db: IconScoreDatabase) -> None:
+        super().__init__(db)
+        self._escrow_data = DictDB(self._ESCROW_DATA, db, value_type=str)
+        self._escrow_count = VarDB(self._ESCROW_COUNT, db, value_type=int)
+        self._tctdapp_addr = VarDB(self._TCTDAPP_ADDR, db, value_type=Address)
+        self._token_addr = VarDB(self._TOKEN_ADDR, db, value_type=Address)
+
+    def on_install(self) -> None:
+        super().on_install()
+
+    def on_update(self) -> None:
+        super().on_update()
+    
+    @external(readonly=True)
+    def hello(self) -> str:
+        Logger.debug(f'Hello, world!', TAG)
+        return "Hello"
+
+    def isOwner(self) -> bool:
+        if self.msg.sender != self.owner:
+            revert("only owner")
+            return False
+        else:
+            return True
+
+    @external
+    def setTokenAddr(self, _addr: Address):
+        if self.isOwner():
+            self._token_addr.set(_addr)
+
+    @external
+    def setTCTAddr(self, _addr: Address):
+        if self.isOwner():
+            self._tctdapp_addr.set(_addr)
+
+    @external
+    def createEscrow(self, _carNumber: str, _price: int, _buyer: Address) -> int:
+        tctDapp = self.create_interface_score(self._tctdapp_addr.get(), tctDappInterface)
+        if self.msg.sender == tctDapp.getCarOwner(_carNumber):
+            revert("only car owner can sell car")
+        if tctDapp.getPersonName(self.msg.sender) == "":
+            revert("buyer is not registed")
+        
+        escrowCount = self._escrow_count.get()
+        escrowCount += 1
+
+        escrowData = {
+            "CARNUMBER" : _carNumber,
+            "PRICE" : _price,
+            "SELLER" : self.msg.sender,
+            "BUYER" : _buyer,
+            "DEPOSITED" : False,
+            "BUYERAPPROVE" : False,
+            "SELLERAPPROVE" : False,
+            "START" : self.now,
+            "lived" : True
+        }
+
+        escrowDataStr = json_dumps(escrowData)
+
+        self._escrow_data[escrowCount] = escrowDataStr
+        self._escrow_count.set(escrowCount)
+        return escrowCount
+
+    
+    @external 
+    def deposit(self, _orderNumber : int):
+        escrowData = json_loads(self._escrow_data[_orderNumber])
+        if escrowData["DEPOSITED"] == True:
+            revert("already DEPOSITED")
+        if escrowData["BUYER"] != self.msg.sender:
+            revert("you are not buyer")
+
+        data = b'called from escrow'
+        token_score = self.create_interface_score(self._token_addr.get(), TokenInterface)
+        token_score._transfer(self.msg.sender,self.address ,escrowData["PRICE"], data)
+
+        escrowData["DEPOSITED"] = True
+        self._escrow_data[_orderNumber] = json_dumps(escrowData)
+
+
+    @external
+    def Approve(self, _orderNumber : int):
+        escrowData = json_loads(self._escrow_data[_orderNumber])
+        if self.msg.sender == escrowData["SELLER"] or self.msg.sender == escrowData["BUYER"]:
+            revert("you are not buyer or seller")
+
+        if self.msg.sender == escrowData["BUYER"] and escrowData["DEPOSITED"] == True:
+            escrowData["SELLERAPPROVE"] = True
+        
+        if self.msg.sender == escrowData["BUYERAPPROVE"]:
+            escrowData["BUYERAPPROVE"] = True
+        
+        if escrowData["SELLERAPPROVE"] and escrowData["BUYERAPPROVE"]:
+            #token transfer to Seller
+            data = b'called from escrow'
+            token_score = self.create_interface_score(self._token_addr.get(), TokenInterface)
+            token_score.transfer(escrowData["SELLER"],escrowData["PRICE"],data)
+
+            #owner change seller to buyer
+            tctDapp = self.create_interface_score(self._tctdapp_addr.get(), tctDappInterface)
+            tctDapp.ownerChange(escrowData["SELLER"], escrowData["BUYER"], escrowData["CARNUMBER"])
+
+        self._escrow_data[_orderNumber] = json_dumps(escrowData)
+
